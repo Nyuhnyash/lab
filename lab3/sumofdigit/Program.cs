7d44366fa3415fae5a45fb89eb47bbec198855fd
Nyuhnyash <37136869+Nyuhnyash@users.noreply.github.com>
2018-10-19 00:06:16 +0400

    Create Program.cs

---
+++ lab3/sumofdigit/Program.cs
@@ -0,0 +1,22 @@
+/* Дата: 18.10.2018 Время: 22:57 */
+using System;
+
+namespace sumofdigit
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine()),
+            	r=0;
+            while (n > 0)
+           	{
+           		r += n % 10;
+           		n /= 10;
+            }
+            Console.WriteLine(r);
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
