06571aee6f95f92d7126038c51f9c48e953ada58
Nyuhnyash <37136869+Nyuhnyash@users.noreply.github.com>
2018-10-22 20:58:23 +0300

    Add files via upload

---
+++ lab3/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Дата: 19.10.2018 Время: 19:33 */
+using System;
+
+namespace perevod
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.Write("Введите основание системы счисления: ");
+			int b = int.Parse(Console.ReadLine()), b2;
+			string s;
+			if (b == 2) {
+				s = "двоичное";
+				b2 = 10;
+			} else {
+				s = "десятичное";
+				b2 = 2;
+			}
+			Console.Write("Введите " + s + " число: ");
+			int num = int.Parse(Console.ReadLine()),
+			res = 0, x = 1;
+			while (num != 0) {
+				res += (num % b2 * x);
+				num /= b2;
+				x *= b;
+			}
+			Console.WriteLine("Результат: " + res);
+			Console.ReadKey(true);
+			Console.Write("Press any key to continue . . . ");
+		}
+	}
+}
