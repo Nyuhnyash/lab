5afc80bf964f94b5e1fdebfdeaddc7b1e55c2263
Nyuhnyash <egor.kuryasev@ya.ru>
2020-05-11 11:33:57 +0400

    Added lab6

---
+++ opengl_lab6/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace opengl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.glControl1 = new OpenTK.GLControl();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.glControl1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(10, 10);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(764, 761);
+            this.panel1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(176, 49);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(161, 176);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = resources.GetString("textBox2.Text");
+            this.textBox2.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(208, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Управление";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
+            this.button1.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.checkBox1.Location = new System.Drawing.Point(117, 24);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(85, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Освещение";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Размер:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(58, 23);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(53, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(764, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Log: ";
+            // 
+            // glControl1
+            // 
+            this.glControl1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.glControl1.BackColor = System.Drawing.Color.Black;
+            this.glControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.glControl1.Location = new System.Drawing.Point(0, 0);
+            this.glControl1.Name = "glControl1";
+            this.glControl1.Size = new System.Drawing.Size(764, 761);
+            this.glControl1.TabIndex = 0;
+            this.glControl1.VSync = false;
+            this.glControl1.Load += new System.EventHandler(this.glControl1_Load);
+            this.glControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.glControl1_Paint);
+            this.glControl1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.glControl1_KeyDown);
+            this.glControl1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.glControl1_MouseDown);
+            this.glControl1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.glControl1_MouseMove);
+            this.glControl1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.glControl1_MouseUp);
+            this.glControl1.MouseWheel += new System.Windows.Forms.MouseEventHandler(this.glControl1_MouseWheel);
+            this.glControl1.Resize += new System.EventHandler(this.glControl1_Resize);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 781);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(10);
+            this.Text = "Lab №6";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private OpenTK.GLControl glControl1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ opengl_lab6/Form1.cs
@@ -0,0 +1,344 @@
+﻿using System;
+using System.Linq;
+using System.Drawing;
+using System.Windows.Forms;
+
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System.Collections.Generic;
+
+namespace opengl
+{
+    public partial class Form1 : Form
+    {
+        float zoom, angleX, angleY, angleZ;
+        const int cubeNumOfFaces = 6;
+        int fov, cubeSize;
+        float elementSize;
+        // B F L R D U
+        readonly Color[] c = { Color.Orange, Color.Red, Color.Green, Color.Blue, Color.Yellow, Color.White};
+        int[,,] ci; // Color Indexes
+        public Form1()
+        {
+            InitializeComponent();
+            Reset();
+        }
+        void Reset()
+        {
+            angleX = angleY = angleZ = 0;
+            fov = 80;
+            cubeSize = (int)numericUpDown1.Value;
+            zoom = -0.5f + (cubeSize - 2) * -0.3f;
+            elementSize = 0.1f;
+            ci = new int[cubeNumOfFaces, cubeSize, cubeSize];
+            for (int i = 0; i < cubeNumOfFaces; i++)
+            {
+                for (int j = 0; j < cubeSize; j++)
+                {
+                    for (int k = 0; k < cubeSize; k++)
+                    {
+                        ci[i, j, k] = i;
+                    }
+                }
+            }
+            textBox1.Text = "Log: ";
+        }
+        private void glControl1_Load(object sender, EventArgs e)
+        {
+            GL.ClearColor(0.9f, 0.9f, 0.9f, 1.0f);
+            GL.Enable(EnableCap.DepthTest);
+            GL.ShadeModel(ShadingModel.Flat);
+            GL.Enable(EnableCap.Normalize);
+            GL.Enable(EnableCap.Light0);
+            GL.Light(LightName.Light0, LightParameter.Position, -Vector4.UnitZ);
+            GL.Light(LightName.Light0, LightParameter.SpotDirection, Vector4.UnitZ);
+        }
+        private void glControl1_Resize(object sender, EventArgs e)
+        {
+            int size = Math.Min(panel1.Width, panel1.Height);
+            glControl1.Width = glControl1.Height = size;
+            GL.Viewport(0, 0, size, size);
+            GL.MatrixMode(MatrixMode.Projection);
+            Matrix4 m = Matrix4.CreatePerspectiveFieldOfView(fov * (float)(Math.PI / 180), 1, 0.1f, 100f);
+            GL.LoadMatrix(ref m);
+            const int o = 20;
+            GL.Frustum(-o, o, -o, o, -o, o);
+        }
+        private void glControl1_Paint(object sender, PaintEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+
+            GL.Translate(0, 0, zoom);
+            // поворот камеры
+            GL.Rotate(angleX, 1, 0, 0);
+            GL.Rotate(angleY, 0, 1, 0);
+            GL.Rotate(angleZ, 0, 0, 1);
+
+            
+
+            // центрирование
+            float fsp = elementSize * cubeSize / 2f;
+            GL.Translate(-fsp, -fsp, -fsp);
+
+            Cube();
+
+            GL.Flush();
+            GL.Finish();
+            glControl1.SwapBuffers();
+        }
+        void Cube()
+        {
+            var s = elementSize;
+            var fs = elementSize * cubeSize; // size of all elements in a row
+            for (int x = 0; x < 2; x++)
+            {
+                // Left & right
+                for (int i = 0; i < cubeSize; i++)
+                {
+                    GL.Begin(PrimitiveType.QuadStrip);
+                    GL.Normal3(x == 0 ? 1 : -1, 0, 0);
+                    GL.Vertex3(fs * x, 0, i * s);
+                    GL.Vertex3(fs * x, 0, (i + 1) * s);
+
+                    for (int j = 1; j <= cubeSize; j++)
+                    {
+                        GL.Material(MaterialFace.FrontAndBack, MaterialParameter.AmbientAndDiffuse, c[ci[x + 2, j - 1, i]]);
+                        GL.Color3(c[ci[x + 2, j - 1, i]]);
+                        GL.Vertex3(fs * x, j * s, i * s);
+                        GL.Vertex3(fs * x, j * s, (i + 1) * s);
+                    }
+                    GL.End();
+                }
+                // Up & down
+                for (int i = 0; i < cubeSize; i++)
+                {
+                    GL.Begin(PrimitiveType.QuadStrip);
+                    GL.Normal3(0, x == 0 ? 1 : -1, 0);
+                    GL.Vertex3(0, fs * x, i * s);
+                    GL.Vertex3(0, fs * x, (i + 1) * s);
+
+                    for (int j = 1; j <= cubeSize; j++)
+                    {
+                        GL.Material(MaterialFace.FrontAndBack, MaterialParameter.AmbientAndDiffuse, c[ci[x + 4, j - 1, i]]);
+                        GL.Color3(c[ci[x + 4, j - 1, i]]);
+                        GL.Vertex3(j * s, fs * x, i * s);
+                        GL.Vertex3(j * s, fs * x, (i + 1) * s);
+                    }
+                    GL.End();
+                }
+                // Front & back
+                for (int i = 0; i < cubeSize; i++)
+                {
+                    GL.Begin(PrimitiveType.QuadStrip);
+                    GL.Normal3(0, 0, x == 0 ? 1 : -1);
+                    GL.Vertex3(0, i * s, fs * x);
+                    GL.Vertex3(0, (i + 1) * s, fs * x);
+
+                    for (int j = 1; j <= cubeSize; j++)
+                    {
+                        GL.Material(MaterialFace.FrontAndBack, MaterialParameter.AmbientAndDiffuse, c[ci[x, j - 1, i]]);
+                        GL.Color3(c[ci[x, j - 1, i]]);
+                        GL.Vertex3(j * s, i * s, fs * x);
+                        GL.Vertex3(j * s, (i + 1) * s, fs * x);
+                    }
+                    GL.End();
+                }
+            }
+        }
+        #region FacesRotation
+        void Turn(int face, bool IsReversed) // поворот грани face в направлении dir часовой стрелки
+        {
+            textBox1.Text += IsReversed ? "' " : " ";
+            switch (face)
+            {
+                case 0: for (int i = 0; i < (IsReversed ? 1 : 3); i++) FrontOrBack(false); break;
+                case 1: for (int i = 0; i < (IsReversed ? 3 : 1); i++) FrontOrBack(true); break;
+                case 2: for (int i = 0; i < (IsReversed ? 3 : 1); i++) LeftOrRight(true); break;
+                case 3: for (int i = 0; i < (IsReversed ? 1 : 3); i++) LeftOrRight(false); break;
+                case 4: for (int i = 0; i < (IsReversed ? 1 : 3); i++) UpOrDown(false); break;
+                case 5: for (int i = 0; i < (IsReversed ? 3 : 1); i++) UpOrDown(true); break;
+            }
+        }
+        void LeftOrRight(bool x)
+        {
+            int x1;
+            if (x)
+            {
+                x1 = 0;
+                Facet(2);
+            } else
+            {
+                x1 = cubeSize - 1;
+                Facet(3);
+            }
+            int[] t = new int[cubeSize] ;
+            for (int i = 0; i < cubeSize; i++)
+                t[i] = ci[0, x1, i];
+            for (int i = 0; i < cubeSize; i++)
+                ci[0, x1, i] = ci[4, x1, cubeSize - 1 - i];
+            for (int i = 0; i < cubeSize; i++)
+                ci[4, x1, i] = ci[1, x1, i];
+            for (int i = 0; i < cubeSize; i++)
+                ci[1, x1, i] = ci[5, x1, cubeSize - 1 - i];
+            for (int i = 0; i < cubeSize; i++)
+                ci[5, x1, i] = t[i];
+        }
+        void UpOrDown(bool x)
+        {
+            int x1;
+            if (x)
+            {
+                x1 = cubeSize - 1;
+                Facet(5);
+            }
+            else
+            {
+                x1 = 0;
+                Facet(4);
+            }
+            int[] t = new int[cubeSize];
+            for (int i = 0; i < cubeSize; i++)
+                t[i] = ci[0, i, x1];
+            for (int i = 0; i < cubeSize; i++)
+                ci[0, i, x1] = ci[2, x1, cubeSize - 1 - i];
+            for (int i = 0; i < cubeSize; i++)
+                ci[2, x1, i] = ci[1, i, x1];
+            for (int i = 0; i < cubeSize; i++)
+                ci[1, i, x1] = ci[3, x1, cubeSize - 1 - i];
+            for (int i = 0; i < cubeSize; i++)
+                ci[3, x1, i] = t[i];
+        }
+        void FrontOrBack(bool x)
+        {
+            int x1;
+            if (x)
+            {
+                x1 = cubeSize - 1;
+                for (int i = 0; i < 3; i++)
+                    Facet(1);
+            }
+            else
+            {
+                x1 = 0;
+                for (int i = 0; i < 3; i++)
+                Facet(0);
+            }
+            int[] t = new int[cubeSize];
+            for (int i = 0; i < cubeSize; i++)
+                t[i] = ci[2, i, x1];
+            for (int i = 0; i < cubeSize; i++)
+                ci[2, i, x1] = ci[4, cubeSize - 1 - i, x1];
+            for (int i = 0; i < cubeSize; i++)
+                ci[4, i, x1] = ci[3, i, x1];
+            for (int i = 0; i < cubeSize; i++)
+                ci[3, i, x1] = ci[5, cubeSize - 1 - i, x1];
+            for (int i = 0; i < cubeSize; i++)
+                ci[5, i, x1] = t[i];
+        }
+        void Facet(int x) // поворот фронтовых элементов грани
+        {
+            int xl = cubeSize - 1;
+            int[] t = new int[xl];
+            for (int i = 0; i < xl; i++)
+                t[i] = ci[x, i, 0];
+            for (int i = 0; i < xl; i++)
+                ci[x, i, 0] = ci[x, 0, xl - i];
+            for (int i = 0; i < xl; i++)
+                ci[x, 0, xl - i] = ci[x, xl - i, xl];
+            for (int i = 0; i < xl; i++)
+                ci[x, xl - i, xl] = ci[x, xl, i];
+            for (int i = 0; i < xl; i++)
+                ci[x, xl, i] = t[i];
+        }
+        /*
+        void Facet3x3(int x) // hardcoded поворот фронта грани куба 3^3
+        {
+            int t2 = ci[x, 0, 0];
+            ci[x, 0, 0] = ci[x, 0, 2];
+            ci[x, 0, 2] = ci[x, 2, 2];
+            ci[x, 2, 2] = ci[x, 2, 0];
+            ci[x, 2, 0] = t2;
+
+            t2 = ci[x, 1, 0];
+            ci[x, 1, 0] = ci[x, 0, 1];
+            ci[x, 0, 1] = ci[x, 1, 2];
+            ci[x, 1, 2] = ci[x, 2, 1];
+            ci[x, 2, 1] = t2;
+        }
+        */
+        #endregion
+
+        private void glControl1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Escape: Application.Exit(); break;
+                case Keys.Space: numericUpDown1.Value = 3; Reset();  break;
+                case Keys.Prior: angleZ += 5; break;
+                case Keys.Next: angleZ -= 5; break;
+
+                case Keys.B: textBox1.Text += "B"; Turn(0, e.Shift); break;
+                case Keys.F: textBox1.Text += "F"; Turn(1, e.Shift); break;
+                case Keys.L: textBox1.Text += "L"; Turn(2, e.Shift); break;
+                case Keys.R: textBox1.Text += "R"; Turn(3, e.Shift); break;
+                case Keys.D: textBox1.Text += "D"; Turn(4, e.Shift); break;
+                case Keys.U: textBox1.Text += "U"; Turn(5, e.Shift); break;
+            }
+            glControl1.Invalidate();
+        }
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            Reset();
+            glControl1.Invalidate();
+        }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+                GL.Enable(EnableCap.Lighting);
+            else
+                GL.Disable(EnableCap.Lighting);
+            glControl1.Invalidate();
+        }
+        private void button1_MouseEnter(object sender, EventArgs e) => textBox2.Visible = true;
+        private void button1_MouseLeave(object sender, EventArgs e) => textBox2.Visible = false;
+        
+        #region MouseRotation
+        bool LMB_is_down = false;
+        int moX, moY;
+        private void glControl1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                LMB_is_down = true;
+                moX = e.X;
+                moY = e.Y;
+            }
+        }
+        private void glControl1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+                LMB_is_down = false;
+        }
+        private void glControl1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (LMB_is_down)
+            {
+                angleY -= e.X - moX;
+                angleX -= e.Y - moY;
+                glControl1.Invalidate();
+
+                moX = e.X;
+                moY = e.Y;
+            }
+        } 
+        #endregion
+        private void glControl1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            zoom += (e.Delta * 0.2f / 120);
+            glControl1.Invalidate();
+        }
+    }
+}
