be267b1716499f8f1e09c0c319a03ede970a9f62
Nyuhnyash <nyuhn@ya.ru>
2022-04-08 19:08:48 +0400

    JS lab 10 reforged

--- js/lab10/script.js
+++ js/lab10/script.js
@@ -1,51 +1,40 @@
 // #1
 function loadData() {
-  const url = "https://473b6556e55372.lhrtunnel.link/games";
-  return fetch(url, {
-    headers: {
-      Authorization: `Basic ${btoa("user:test")}`,
-    },
-  }).then((r) => r.json());
+    const username = 'user';
+    const password = 'test';
+
+    const url = 'https://0711-46-20-75-201.eu.ngrok.io/games';
+    return fetch(url, {
+        headers: {
+            Authorization: `Basic ${btoa(`${username}:${password}`)}`,
+        },
+    }).then(r => r.json());
 }
 
 // #4
+const table = document.querySelector('table > tbody');
+const rowTemplate = document.querySelector('template').content.querySelector('tr');
 function updateTable(data) {
-  const table = document.getElementsByTagName("tbody")[0];
-  console.log(table);
-  for (const e of data) {
-    const game = e.gameBase;
-    const row = document.createElement("tr");
-    const image = document.createElement("td");
-    const img = document.createElement("img");
-    img.src = game.image;
-    img.width = 150;
-    image.appendChild(img);
+    for (const game of data) {
+        const row = rowTemplate.cloneNode(true);
+        table.appendChild(row);
 
-    const name = document.createElement("td");
-    const a = document.createElement("a");
-    const link = new URL(game.link);
-
-    a.href = "https://store.epicgames.com/ru/p" + link.pathname;
-    a.innerText = game.name;
-    name.appendChild(a);
-
-    const price = document.createElement("td");
-    price.innerText = game.price === 0 ? "БЕСПЛАТНО" : game.price + '₽';
-
-    const discountPrice = document.createElement("td");
-    discountPrice.innerText = game.discountPrice === 0 
-    ? "БЕСПЛАТНО" 
-    : game.discountPrice + '₽';
-
-    row.appendChild(image);
-    row.appendChild(name);
-    row.appendChild(price);
-    row.appendChild(discountPrice);
-    table.appendChild(row);
-  }
+        const img = row.querySelector('.game-img');
+        const link = row.querySelector('.game-link');
+        const price = row.querySelector('.game-price');
+        const priceDiscount = row.querySelector('.game-price-discount');
+        const discount = row.querySelector('.game-discount');
+        
+        img.src = game.image;
+        link.href = game.link;
+        link.innerText = game.name;
+        price.innerText = game.price === 0 ? 'БЕСПЛАТНО' : game.price + '₽';
+        priceDiscount.innerText = game.discountPrice === 0 ? 'БЕСПЛАТНО' : game.discountPrice + '₽';
+        discount.innerText = game.discountPercent + '%';
+    }
 }
 
 (async () => {
-  const data = await loadData();
-  updateTable(data);
+    const data = await loadData();
+    updateTable(data);
 })();
