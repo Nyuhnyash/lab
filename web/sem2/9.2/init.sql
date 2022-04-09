e70e69be5c5e68981bbfcde091748a685129333b
Nyuhnyash <git@mailing.cf>
2022-04-09 12:02:45 +0400

    Web lab 9

---
+++ web/sem2/9.2/init.sql
@@ -0,0 +1,11 @@
+﻿create database task9;
+use task9;
+create table students(
+    id int auto_increment primary key,
+    fio varchar(250),
+    course int unsigned,
+    birthdate date,
+    email varchar(250),
+    phone varchar(250),
+    details text
+);
---
+++ web/sem2/9.2/select.php
@@ -0,0 +1,44 @@
+﻿<form>
+    <select name="field">
+        <option value=""></option>
+        <option value="birthdate">День рождения</option>
+        <option value="course">Курс</option>
+    </select>
+    <select name="order">
+        <option value="asc">По возрастанию</option>
+        <option value="desc">По убыванию</option>
+    </select>
+    <input name="course" type="range" min="1" max="5">
+    <input type="submit">
+</form>
+
+<?php
+if (!is_null($_GET['field'])) {
+    $field = $_GET['field'];
+    $order = $_GET['order'];
+    $conn = mysql_connect("localhost", "root", "") or 
+        die("Невозможно установить соединение: ". mysql_error());
+    
+    mysql_select_db('task9', $conn);
+    $fields = empty($field) ? "*" : "fio, $field";
+    
+    $sql = "select $fields from students";
+
+    if ($field == 'course') {
+        $course = $_GET['course'];
+        $sql = $sql . " where course = $course";
+    }
+
+    $order_field = empty($field) ? "fio" : $field;
+    $sql = $sql . " order by $order_field $order";
+    
+    $response = mysql_query($sql, $conn);
+    if (!$response) {
+        var_dump(mysql_error());
+        die;
+    }
+    while($row = mysql_fetch_array($response, MYSQL_ASSOC)) {
+        print_r($row);
+    }
+    mysql_close($conn);
+}
