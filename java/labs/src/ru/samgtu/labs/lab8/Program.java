bdd139365de63715fa0af81c193609efc52b3056
Nyuhnyash <nyuhn@ya.ru>
2023-02-27 11:32:51 +0400

    java lab8 task2

--- java/labs/src/ru/samgtu/labs/lab8/Program.java
+++ java/labs/src/ru/samgtu/labs/lab8/Program.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Program {
     public static void main(String[] args) throws IOException {
@@ -20,5 +22,51 @@ public class Program {
                 .distinct()
                 .sorted()
                 .toList();
+
+
+        names.stream().filter(name -> name
+                .chars()
+                .mapToObj(i -> (char) i)
+                .filter(Program::isVowel)
+                .count() == 3
+        );
+
+        names.stream().filter(name -> name
+                .chars()
+                .mapToObj(i -> (char)i)
+                .filter(Program::isVowel)
+                .count() == 4
+        );
+
+
+        names.stream().filter(name -> name
+                .chars()
+                .mapToObj(i -> (char)i)
+                .filter(Program::isVowel)
+                .count() == 5
+        );
+
+        names.stream().min(Comparator.comparing(String::length));
+        names.stream().max(Comparator.comparing(String::length));
+
+        names.stream()
+                .filter(name -> name.startsWith("Е"))
+                .findFirst();
+
+        names.stream().allMatch(s -> s
+                .chars()
+                .mapToObj(i -> (char) i)
+                .anyMatch(Program::isVowel)
+        );
+
+        names.stream().collect(Collectors.toMap(s -> s, String::length));
+
+        names.stream().collect(Collectors.groupingBy(String::length));
+
+        names.stream().collect(Collectors.partitioningBy(s -> isVowel(s.charAt(0))));
+    }
+
+    private static boolean isVowel(char c) {
+        return "аеёиоуыэюя".contains(String.valueOf(c).toLowerCase());
     }
 }
